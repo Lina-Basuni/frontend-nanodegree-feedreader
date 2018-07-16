d0c30b99ce79e315e741458519433d48becb20fa
Lina Ahmed Kamal Basuni <lina.ahmedkamal@gmail.com>
2018-07-16 12:26:30 +0200

    test that .feed container contains atleast one entry within it

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -64,7 +64,7 @@ $(function() {
        * the CSS to determine how we're performing the
        * hiding/showing of the menu element.
        */
-       it('Menu is hidden by default',function(){
+       it('is hidden by default',function(){
          var check = $('body').hasClass("menu-hidden")
          expect(check).toBe(true);
        });
@@ -75,7 +75,7 @@ $(function() {
         * clicked and does it hide when clicked again.
         */
 
-         it('Menu toggles on click',function(){
+         it('toggles on click',function(){
 
            $('.menu-icon-link').click();
            expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -91,7 +91,12 @@ $(function() {
 
 
   /*  /* TODO: Write a new test suite named "Initial Entries" */
-  //  describe('Initial Entries',function(){
+    describe('Initial Entries',function(){
+      beforeEach(function(done){
+        loadFeed(0,done)
+
+      });//end before each
+
 
       /* TODO: Write a test that ensures when the loadFeed
        * function is called and completes its work, there is at least
@@ -99,9 +104,14 @@ $(function() {
        * Remember, loadFeed() is asynchronous so this test will require
        * the use of Jasmine's beforeEach and asynchronous done() function.
        */
+       it('have at least one .entry within the .feed container',function(done){
+         expect($(".feed > a").length).not.toBe(0)
+         done()
+
+       });
 
 
-  //  });
+    });
 
 
 
